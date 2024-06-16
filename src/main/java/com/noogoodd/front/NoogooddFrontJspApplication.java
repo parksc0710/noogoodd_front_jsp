8a5517c62df407769c7dc30a67b72bb97ac3fa46
Seong Cheol Park <parksc0711@gmail.com>
2024-06-16 23:24:38 +0900

    jsp init

---
+++ src/main/java/com/noogoodd/front/NoogooddFrontJspApplication.java
@@ -0,0 +1,13 @@
+package com.noogoodd.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoogooddFrontJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoogooddFrontJspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noogoodd/front/NoogooddFrontJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noogoodd.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoogooddFrontJspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
